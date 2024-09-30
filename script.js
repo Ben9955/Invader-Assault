fde2afe4a1cf46235f0d2502194760947a943891
ben9955 <ben95ansari@gmail.com>
2024-09-30 15:22:53 +0100

    Initial commit - Added Space Invader project files

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-  const canvas = document.getElementById("myCanvas");
-  const ctx = canvas.getContext("2d");
-  canvas.width = 600;
-  canvas.height = window.innerHeight * 0.95;
-  const game = new Game(canvas);
-});
