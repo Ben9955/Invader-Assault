4fcdf5df4baefeee945dae18447fb7ddc00022ba
ben9955 <ben95ansari@gmail.com>
2024-09-30 15:15:15 +0100

    Initial commit - created initial files

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 600;
+  canvas.height = window.innerHeight * 0.95;
+  console.log(ctx);
+  const game = new Game(canvas);
+});
