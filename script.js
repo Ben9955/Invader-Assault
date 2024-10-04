0bc391d8ef883ff96a5959dabb44a16c534c4cdd
ben9955 <ben95ansari@gmail.com>
2024-10-04 11:31:49 +0100

    Introduced `Background` class with vertical scrolling feature for continuous movement.
    Integrated background update and rendering into the main game loop.

--- script.js
+++ script.js
@@ -126,11 +126,47 @@ class Shooter {
   }
 }
 
+class Background {
+  constructor(game) {
+    this.game = game;
+    this.image = new Image();
+    this.image.src = "./images/background_2.jpeg"; // Your space background image
+    this.width = this.game.canvas.width;
+    this.height = this.game.canvas.height;
+
+    // Y position for vertical scrolling
+    this.y1 = 0;
+    this.y2 = -this.height; // Second background placed above the first one for looping effect
+    this.speed = 2; // Scrolling speed
+  }
+
+  draw(context) {
+    // Draw two images: one on screen and one above the screen to simulate continuous scrolling
+    context.drawImage(this.image, 0, this.y1, this.width, this.height);
+    context.drawImage(this.image, 0, this.y2, this.width, this.height);
+  }
+
+  update() {
+    // Move both background images downward (or upward if you want reverse scrolling)
+    this.y1 += this.speed;
+    this.y2 += this.speed;
+
+    // When the first image moves completely off the screen, reset its position
+    if (this.y1 >= this.height) {
+      this.y1 = -this.height;
+    }
+    if (this.y2 >= this.height) {
+      this.y2 = -this.height;
+    }
+  }
+}
+
 class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.width = canvas.width;
     this.height = canvas.height;
+    this.background = new Background(this);
 
     this.shooter = new Shooter(this);
     this.projectiles = [];
@@ -164,6 +200,9 @@ class Game {
   }
 
   render(context) {
+    this.background.update(); // Update background position
+    this.background.draw(context); // Draw the scrolling background
+
     this.projectiles.forEach((projectile) => {
       projectile.draw(context);
       projectile.move();
